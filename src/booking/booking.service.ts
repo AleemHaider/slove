3a67bd457a18049c9701b5c3f99d083d3f230df2
AleemHaider <61236354+AleemHaider@users.noreply.github.com>
2023-11-22 16:40:17 +0500

    update contract api created

--- src/booking/booking.service.ts
+++ src/booking/booking.service.ts
@@ -235,7 +235,7 @@ export class BookingService {
       }
       eventEntity.contractDiscription=dto.contractDiscription;
       
-      this.logger.error(dto.isTicketClose);
+      
       
       
 
